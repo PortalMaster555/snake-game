8a665e1000c571ac8cb52f404ad1ac3d16342917
PortalMaster555 <portalmaster55@gmail.com>
2022-06-10 21:25:03 -0500

    game WIP, does not compile rn

--- snake.c
+++ snake.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #ifdef _WIN32
     #include <windows.h>
@@ -7,7 +8,25 @@
 #endif
 
 float framesPerSecond = 60;
+int screenX = 10
+int screenY = 10
 
+bool gameLoop = false;
+
+int **snakePos;
+int *applePos;
+
+int main(int argc, char **argv)
+{
+	gameLoop = true;
+	while(gameLoop)
+	{
+        	delay(1.0 / framesPerSecond);
+	}
+    return 0;
+}
+
+//TIME FUNCTION
 void delay(float seconds)
 {   // Pretty crossplatform, both ALL POSIX compliant systems AND Windows
     int milliSeconds = 1000 * seconds;
@@ -17,15 +36,7 @@ void delay(float seconds)
         	usleep(1000 * milliSeconds); //usleep uses microseconds
 	#endif
 }
- 
-int main(int argc, char **argv)
-{
-	int i;
-	for (i = 0; i < 360; i++)
-	{
-		printf("%f: ", 1.0 / framesPerSecond);
-        	delay(1.0 / framesPerSecond);
-        	printf("Frame %d\n", i + 1);
-    	}
-    return 0;
-}
+
+//GAME FUNCTIONS
+
+void printGrid(snakePos, applePos
