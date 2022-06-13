4e870c5d63a3173e63edfecc3cdd7a2619e28df6
PortalMaster555 <portalmaster55@gmail.com>
2022-06-12 19:51:09 -0500

    refactoring using realloc, realized it does what i need

--- snake.c
+++ snake.c
@@ -33,28 +33,18 @@ int main(int *argc, char **argv)
 	int (*snakePos)[2];
 	int (*snakePosStorage)[2];
 	
-	snakePos = malloc(currentLength * 2);
-	snakePosStorage = malloc(currentLength * 2);
+	snakePos = (int(*)[2])malloc(currentLength * 2);
 
-	snakePos[0][0] = 2;
-	snakePos[0][1] = 3;
+	snakePos[0][0] = 42069;
+	snakePos[0][1] = 69420;
 
-	memcpy(snakePosStorage, snakePos, sizeof(snakePos)); 
-	free(snakePos);	
 	for(int i = 0; i < 5; i++)
-	{	
-		memcpy(snakePos, snakePosStorage, sizeof(snakePos));
-
+	{
 		for(int s = 0; s < currentLength; s++)
 		{
-			printf("(%d, %d)\n", snakePos[s][0], snakePos[s][1]);	
+			printf("Pos. %d: (%d, %d)", s, snakePos[s][0], snakePos[s][1]);
 		}
 		printf("\n");
-	
-		snakePosStorage = malloc((currentLength + 1) * 2);
-		memcpy(snakePosStorage, snakePos, sizeof(snakePos)); //the snake now has an extra [x,y] set to use
-				currentLength++;
-		free(snakePos);
 	}
 	return 0;
 }
