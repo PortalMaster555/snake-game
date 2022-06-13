cfedb22ede5e43958cc602e4080d6a80338b6817
PortalMaster555 <portalmaster55@gmail.com>
2022-06-12 22:49:44 -0500

    GRID PRINTSgit add .git add .!

--- snake.c
+++ snake.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 #ifdef _WIN32
 	#include <windows.h>
@@ -11,9 +12,58 @@
 
 //FUNCTIONS
 
-void printGrid()
+void printGrid(int applePos[2], int (*snakePos)[2],int dims[2], char chars[4])
+{ // . @ O o
+	for(int y = 0; y < dims[1]; y++)
+	{
+		for(int x = 0; x < dims[0]; x++)
+		{
+				if(x == applePos[0] && y == applePos[1])
+				{
+					printf("%c ", chars[1]);
+				}
+				else if(x == snakePos[0][0] && y == snakePos[0][1])
+				{
+					printf("%c ", chars[2]);
+				}
+				else //nested ifs are disgusting
+				{	
+					bool isPrinted = false;
+					for(int s = 0; s < sizeof(snakePos) / 2; s++)
+					{
+						if(x == snakePos[s][0] && y == snakePos[s][1] && !isPrinted)
+						{
+							
+							printf("%c ", chars[3]);
+							isPrinted = true;
+						}
+						else if(!isPrinted)
+						{
+							printf("%c ", chars[0]);
+							isPrinted = true;
+						}
+					}
+				}
+		}
+		printf("\n");
+	}
+}
+
+void growSnake(int (*snakePos)[2], int *p)
 {
-	
+		printf("\n");
+		*p = *p + 1;
+		snakePos = realloc(snakePos, *p * 2 * sizeof(int));
+		snakePos[*p - 1][0] = *p;
+		snakePos[*p - 1][1] = *p;
+}
+
+void printCurrentSnake(int (*snakePos)[2], int *p)
+{
+	for(int s = 0; s < *p; s++)
+	{
+		printf("Pos. %d: (%d, %d)\n", s, snakePos[s][0], snakePos[s][1]);
+	}
 }
 
 //VARIABLES
@@ -26,37 +76,24 @@ void printGrid()
 
 int main(int *argc, char **argv)
 {
-	bool isGameOver = false;
+	int screenSize[2] = {11, 11};
+	char charArray[4] = {'.', '@', 'O', 'o'};
+
 	int currentLength = 1;
-
-	int applePos[2] = {0, 0};
+	int *lengthPointer;
+	lengthPointer = &currentLength;
 	int (*snakePos)[2];
-	int (*snakePosStorage)[2];
+	int applePos[2] = {5, 5}; //magic number
+
+	snakePos = calloc(1, 2 * sizeof(int));
+	snakePos[0][0] = 1;
+	snakePos[0][1] = 1;
 	
-	snakePos = (int(*)[2])malloc(currentLength * 2);
-
-	snakePos[0][0] = 42069;
-	snakePos[0][1] = 69420;
-
-	for(int i = 0; i < 5; i++)
-	{
-		for(int s = 0; s < currentLength; s++)
-		{
-			printf("Pos. %d: (%d, %d)\n", s, snakePos[s][0], snakePos[s][1]);
-		}
-		printf("\n");
-		currentLength++;
-		printf("Current Length used in next realloc: %d\n", currentLength);
-		snakePos = realloc(snakePos, currentLength);
-		if(snakePos == NULL)
-		{
-			printf("Narupo GAH");
-		}
-		else
-		{
-		snakePos[currentLength - 1][0] = currentLength;
-		snakePos[currentLength - 1][1] = currentLength;
-		}
-	}
+	printGrid(applePos, snakePos, screenSize, charArray);
+	
+	//growSnake(snakePos, lengthPointer);
+	//printCurrentSnake(snakePos, lengthPointer);
+	
+	free(snakePos);
 	return 0;
 }
