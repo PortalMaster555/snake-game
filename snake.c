0e4b724926a574975c3d664596fbe436df7c89a4
PortalMaster555 <portalmaster55@gmail.com>
2022-06-12 19:26:28 -0500

    compiles, but with illegal mem access

--- snake.c
+++ snake.c
@@ -11,7 +11,10 @@
 
 //FUNCTIONS
 
-
+void printGrid()
+{
+	
+}
 
 //VARIABLES
 
@@ -23,10 +26,35 @@
 
 int main(int *argc, char **argv)
 {
+	bool isGameOver = false;
+	int currentLength = 1;
+
 	int applePos[2] = {0, 0};
+	int (*snakePos)[2];
+	int (*snakePosStorage)[2];
+	
+	snakePos = malloc(currentLength * 2);
+	snakePosStorage = malloc(currentLength * 2);
 
-	//memcpy(applePos, newApplePos, sizeof(applePos));
+	snakePos[0][0] = 2;
+	snakePos[0][1] = 3;
 
-	printf("(%d, %d)\n", applePos[0], applePos[1]);
+	memcpy(snakePosStorage, snakePos, sizeof(snakePos)); 
+	free(snakePos);	
+	for(int i = 0; i < 5; i++)
+	{	
+		memcpy(snakePos, snakePosStorage, sizeof(snakePos));
+
+		for(int s = 0; s < currentLength; s++)
+		{
+			printf("(%d, %d)\n", snakePos[s][0], snakePos[s][1]);	
+		}
+		printf("\n");
+	
+		snakePosStorage = malloc((currentLength + 1) * 2);
+		memcpy(snakePosStorage, snakePos, sizeof(snakePos)); //the snake now has an extra [x,y] set to use
+				currentLength++;
+		free(snakePos);
+	}
 	return 0;
 }
